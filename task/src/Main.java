504dc4ac2862dd4a64a4cc173587a02a1ae11c49
Koki Yamazaki <koki.yamazaki@justsystems.com>
2019-09-24 11:31:05 +0900

    spec1: 出力文字を変数に書き出す

--- task/src/Main.java
+++ task/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
+
+    private static final String TASK1_OUTPUT = "上手";
+
     public static void main(String args[]){
-        System.out.print("上手");
+        System.out.print(TASK1_OUTPUT);
     }
 }
