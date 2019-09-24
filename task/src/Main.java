6e2b1f2c1bcce0e2dc30b216999c7a8c3c851db7
Koki Yamazaki <koki.yamazaki@justsystems.com>
2019-09-24 13:32:05 +0900

    spec3: プログラム実行時に受け取った引数文字列を表示するようにする

--- task/src/Main.java
+++ task/src/Main.java
@@ -2,11 +2,17 @@ public class Main {
 
     private static final String TASK1_OUTPUT = "上手";
 
-    public static void main(String args[]){
-        printDictionaryData();
+    public static void main(String args[]) {
+
+        if (args.length != 1){
+            System.out.println("データファイルのパスを入力して下さい");
+            System.exit(1);
+        }
+
+        printDictionaryData(args[0]);
     }
 
-    private static void printDictionaryData() {
-        System.out.print(TASK1_OUTPUT);
+    private static void printDictionaryData(String str) {
+        System.out.print(str);
     }
 }
