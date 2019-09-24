b453929de4b5242d7f1f4c6becc30eeb42472c08
Koki Yamazaki <koki.yamazaki@justsystems.com>
2019-09-24 11:32:42 +0900

    spec1: 課題1のタスクをメソッドに切り出す

--- task/src/Main.java
+++ task/src/Main.java
@@ -3,6 +3,10 @@ public class Main {
     private static final String TASK1_OUTPUT = "上手";
 
     public static void main(String args[]){
+        runTask1();
+    }
+
+    public static void runTask1(){
         System.out.print(TASK1_OUTPUT);
     }
 }
